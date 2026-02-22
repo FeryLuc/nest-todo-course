910a05201a9aac481f88421ba83836a79bd78931
GrasPanda <Lucasus822@gmail.com>
2026-02-22 16:10:49 +0100

    concept: CONFIG-MODULE - lecture de variables d'env avec ConfigService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // Retourne le nom de l'app et le port depuis les variables d'environnement
+  @Get('config')
+  getConfig() {
+    return this.appService.getConfig();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  // ConfigService est injecté automatiquement grâce à isGlobal: true
+  constructor(private configService: ConfigService) {}
+
+  getConfig() {
+    return {
+      // configService.get() lit la variable depuis process.env (via le .env chargé)
+      appName: this.configService.get<string>('APP_NAME', 'nest-todo-course'),
+      port: this.configService.get<number>('PORT', 3000),
+      nodeEnv: this.configService.get<string>('NODE_ENV', 'development'),
+      dbHost: this.configService.get<string>('DB_HOST'),
+    };
   }
 }
